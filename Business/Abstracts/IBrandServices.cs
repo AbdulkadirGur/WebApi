7876b30408bc76338d5ee86290b0de507832b2a7
Abdulkadir Gür <58683068+AbdulkadirGur@users.noreply.github.com>
2024-02-25 22:05:02 +0300

    Add project files.

---
+++ Business/Abstracts/IBrandServices.cs
@@ -0,0 +1,19 @@
+﻿using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstracts
+{
+    public interface IBrandServices
+    {
+        CreatedBrandResponse Add(CreateBrandRequest createBrandRequest);
+
+        List <Brand> GetAll();
+
+    }
+}
---
+++ Business/Concretes/BrandManager.cs
@@ -0,0 +1,47 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    internal class BrandManager : IBrandServices
+    {
+        private readonly IBrandServices _brandDal;
+
+        public BrandManager(IBrandServices brandDal)
+        {
+            _brandDal = brandDal;
+        }
+
+        public CreatedBrandResponse Add(CreateBrandRequest createBrandRequest)
+        {
+            //business rules
+
+            //mapping ----Autoamapper
+            Brand brand = new();
+            brand.Name = createBrandRequest.Name;
+            brand.CreatedDate = DateTime.Now;
+
+            _brandDal.Add(brand);
+
+            //mapping
+            CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
+            createdBrandResponse.Name = brand.Name;
+            createdBrandResponse.Id = 4;
+            createdBrandResponse.CreatedDate = brand.CreatedDate;
+
+            return createdBrandResponse;
+        }
+
+        public List<Brand> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
